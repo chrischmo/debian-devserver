eafae8cf7a0135cc6ad75e693f0efccd5a18b9b2
Christian Schmitt <chrischmo@gmail.com>
2014-03-29 22:33:45 +0000

    Corrected the permissions for the key files and removed the add-ssh part because it doesn't work automatically (passphrase has to be entered

--- bootstrap.sh
+++ bootstrap.sh
@@ -33,7 +33,7 @@ chsh -s /bin/zsh vagrant
 # Howto: http://blog.sanctum.geek.nz/managing-dot-files-with-git/
 # Clone dotfiles
 git clone git://github.com/chrischmo/dotfiles.git ~vagrant/.dotfiles
-chown vagrant: ~vagrant/.dotfiles/ida*
+chown vagrant: ~vagrant/.dotfiles/*
 # Clean up possibly existing dotfiles
 rm -r .vim .vimrc .gitconfig .zshrc
 # Create links in homefolder
@@ -45,6 +45,10 @@ chown vagrant: ~vagrant/.zshrc ~vagrant/.vimrc ~vagrant/.vim ~vagrant/.gitconfig
 
 # Copy SSH keys from /vagrant
 cp /vagrant/.ssh_keys/id_rsa* ~vagrant/.ssh
-chown vagrant: ~vagrant/.ssh/id_rsa*
+chown vagrant: .ssh/id_rsa*
+chmod 600 ~vagrant/.ssh/id_rsa
+chmod 644 ~vagrant/.ssh/id_rsa.pub
+# TODO Manually add key to ssh-agent (doesn't work automatically bc passphrase)
 # Workaround for "Could not open a connection to your authentication agent."
-eval $(ssh-agent)
+# eval $(ssh-agent)
+# ssh-add ~vagrant/.ssh/id_rsa
