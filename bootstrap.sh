a583b6922c75da4e477e855e2e8eb842dd0da80e
Christian Schmitt <chrischmo@gmail.com>
2014-03-30 01:33:55 +0000

    Added Setup of dotfiles repository URL, corrected error in removal possible pre-existing dotfiles, added message regarding dotfiles submodule updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -33,8 +33,12 @@ chsh -s /bin/zsh vagrant
 # Howto: http://blog.sanctum.geek.nz/managing-dot-files-with-git/
 # Clone dotfiles
 git clone git://github.com/chrischmo/dotfiles.git ~vagrant/.dotfiles
-chown vagrant: ~vagrant/.dotfiles/*
+chown -R vagrant: ~vagrant/.dotfiles
+# Set up dotfiles repository URL correctly to allow for pushing to it later
+cd ~vagrant/.dotfiles
+git remote set-url origin git@github.com:chrischmo/dotfiles.git
 # Clean up possibly existing dotfiles
+cd ~vagrant/
 rm -r .vim .vimrc .gitconfig .zshrc
 # Create links in homefolder
 ln -s ~vagrant/.dotfiles/vim ~vagrant/.vim
@@ -51,11 +55,18 @@ chmod 644 ~vagrant/.ssh/id_rsa.pub
 
 # Final Messages:
 echo -e \
-	"======================= TODO POST-INSTALL: ========================="
+	"======================= TODO POST-INSTALL: =========================\n"
 echo -e "Manually add the SSH key to ssh-agent (can't be done automatically \n\
 because passphrase has to be entered). Execute the following command \n\
 first to prevent the 'Could not open a connection to your \n\
 authentication agent.' error:"
 echo -e "    eval <DOLLAR>(ssh-agent) \n\
-    ssh-add ~vagrant/.ssh/id_rsa"
+    ssh-add ~vagrant/.ssh/id_rsa\n"
+
+echo -e "--------------------------------------------------------------------"
+
+echo -e "Option: Update the vim plugins by cd-ing into .dotfiles and running:"
+echo -e "    git submodule update \n\
+    git submodule foreach git submodule init \n\
+    git submodule foreach git submodule update\n"
 echo -e "===================================================================="
