801e7122aaeabe43bc49e3621815e04d4eae0000
Christian Schmitt <chrischmo@gmail.com>
2014-04-02 14:10:37 +0200

    Added submodule update procedure for dotfiles.

--- bootstrap.sh
+++ bootstrap.sh
@@ -33,10 +33,16 @@ chsh -s /bin/zsh vagrant
 # Howto: http://blog.sanctum.geek.nz/managing-dot-files-with-git/
 # Clone dotfiles
 git clone git://github.com/chrischmo/dotfiles.git ~vagrant/.dotfiles
-chown -R vagrant: ~vagrant/.dotfiles
 # Set up dotfiles repository URL correctly to allow for pushing to it later
 cd ~vagrant/.dotfiles
 git remote set-url origin git@github.com:chrischmo/dotfiles.git
+# Initialize & update submodules
+git submodule init
+git submodule update
+git submodule foreach git submodule init
+git submodule foreach git submodule update
+# Set correct permissions for dotfiles-folder
+chown -R vagrant: ~vagrant/.dotfiles
 # Clean up possibly existing dotfiles
 cd ~vagrant/
 rm -r .vim .vimrc .gitconfig .zshrc
@@ -45,6 +51,7 @@ ln -s ~vagrant/.dotfiles/vim ~vagrant/.vim
 ln -s ~vagrant/.dotfiles/vimrc ~vagrant/.vimrc
 ln -s ~vagrant/.dotfiles/gitconfig ~vagrant/.gitconfig
 ln -s ~vagrant/.dotfiles/zshrc ~vagrant/.zshrc
+# Set correct permissions for links to dotfiles
 chown vagrant: ~vagrant/.zshrc ~vagrant/.vimrc ~vagrant/.vim ~vagrant/.gitconfig
 
 # Copy SSH keys from /vagrant
@@ -54,8 +61,7 @@ chmod 600 ~vagrant/.ssh/id_rsa
 chmod 644 ~vagrant/.ssh/id_rsa.pub
 
 # Final Messages:
-echo -e \
-	"\n======================= To-Do POST-INSTALL: =========================\n"
+echo -e "\n======================= To-Do POST-INSTALL: =========================\n"
 echo -e "Manually add the SSH key to ssh-agent (can't be done automatically \n\
 because passphrase has to be entered). Execute the following command \n\
 first to prevent the 'Could not open a connection to your \n\
@@ -63,12 +69,9 @@ authentication agent.' error:"
 echo -e "    eval <DOLLAR>(ssh-agent) \n\
     ssh-add ~vagrant/.ssh/id_rsa\n"
 
-echo -e "--------------------------------------------------------------------\n"
+echo -e "---------------------------------------------------------------------\n"
 
-echo -e "OPTIONAL: Update the vim plugins by cd-ing into .dotfiles and running:"
-echo -e "    git submodule init \n\
-    git submodule update \n\
-    git submodule foreach git submodule init \n\
-    git submodule foreach git submodule update \n\
-    git submodule foreach git pull origin master"
+echo -e "OPTIONAL: Update the vim plugins by cd-ing into .dotfiles & running:"
+echo -e "    git submodule foreach git pull origin master"
+echo -e "    git push"
 echo -e "===================================================================="
